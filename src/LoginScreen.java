0be59daadfe3af540ac086f868e04256c9834bf5
Серёга <zizubest@meta.ua>
2015-03-04 13:10:02 +0200

    Initial commit

---
+++ src/LoginScreen.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+
+public class LoginScreen {
+    JFrame frame =new JFrame("Welcome");
+
+    JPanel panel = new JPanel();
+    JLabel loginLabel = new JLabel("Input Login");
+    JLabel passLabel = new JLabel("Input password");
+    JTextField loginField = new JTextField();
+    JTextField passField = new JTextField();
+    JButton okButton = new JButton();
+
+
+
+
+}
